1925c38cd916bd970167987ca0a857f56f8c6e0a
Bryanconect <148644042+Bryanconect@users.noreply.github.com>
2023-11-09 18:51:58 -0300

    Update template.blade.php
    Signed-off-by: Bryanconect <148644042+Bryanconect@users.noreply.github.com>

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -51,7 +51,7 @@
         <ul class="list-unstyled small">
           <li class="mb-2">Em busca de solução?</li>
           <li class="mb-2">Aqui tem a documentação!</li>
-          <li class="mb-2">Código de Licença <a href="https://github.com/twbs/bootstrap/blob/main/LICENSE" target="_blank" rel="license noopener">MIT</a></li>
+          <li class="mb-2">Código de Licença <a href="https://github.com/Bryanconect/ConnectRequisitos" target="_blank" rel="license noopener">MIT</a></li>
           <li class="mb-2">Versão: v1.0</li>
         </ul>
       </div>
