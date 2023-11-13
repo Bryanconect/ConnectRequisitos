48ba37417e93e628412c6a8675ee60ef4cb9ad8a
Bryanconect <148644042+Bryanconect@users.noreply.github.com>
2023-11-13 20:40:00 -0300

    Update indexgestaorequisito.blade.php
    Signed-off-by: Bryanconect <148644042+Bryanconect@users.noreply.github.com>

--- resources/views/indexgestaorequisito.blade.php
+++ resources/views/indexgestaorequisito.blade.php
@@ -15,6 +15,12 @@ $userelicitacao = auth()->user()->id;
         </a>
 </div>
 
+@if(session('mensagem'))
+    <div class="text-center mt-3 mb-4 alert alert-success">
+        <p>{{session('mensagem')}}</p>
+    </div>
+@endif 
+
 <h1 class="text-center">Gerenciar Programas</h1>
 
 <div class="col-8 m-auto"> 
